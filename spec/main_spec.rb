2df1213c9b6386cbb7cecafbb072384ff90fe5b5
Stella Miranda <stellamariam@gmail.com>
2018-08-06 03:15:50 -0500

    implement load Customer and load Invoices on Main

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -9,15 +9,16 @@ describe Main do
 
   before :each do 
 
-    @invoices_params = [{ "debtor"=>"fritz-kola", "number"=>"inv-kola1", "amount"=>16000}, {"debtor"=>"Tommi's Burger Joint", "number"=>"inv-burger1", "amount"=>15000}, {"debtor"=>"fritz-kola", "number"=>"inv-kola2", "amount"=>14000}, {"debtor"=>"Dolores", "number"=>"inv-burrito1", "amount"=>15000}, {"debtor"=>"Tommi's Burger Joint", "number"=>"inv-burger2", "amount"=>2000 }]
-    @customer_params = { "customer" => 40000 }
+    @unique_invoice_params = [{"debtor"=>"burgermeister", "number"=>"inv-burger3", "amount"=>18000}]
+    @invoices_params = [{"debtor"=>"fritz-kola", "number"=>"inv-kola1", "amount"=>16000}, {"debtor"=>"Tommi's Burger Joint", "number"=>"inv-burger1", "amount"=>15000}, {"debtor"=>"fritz-kola", "number"=>"inv-kola2", "amount"=>14000}, {"debtor"=>"Dolores", "number"=>"inv-burrito1", "amount"=>15000}, {"debtor"=>"Tommi's Burger Joint", "number"=>"inv-burger2", "amount"=>2000}]
+    @customer_params = {"customer"=>40000}
     
   end
 
   describe "load_customer" do
 
     it "should return an array" do
-      expect(load_customer(@customer_params)).to be_an_instance_of(Array)
+      expect(load_customer(@customer_params)).to be_an_instance_of(Customer)
     end
 
   end
@@ -28,8 +29,28 @@ describe Main do
       expect(load_invoices(@invoices_params)).to be_an_instance_of(Array)
     end
 
-
+    it "should return an array of Invoices" do
+      expect(load_invoices(@invoices_params)).to all(be_an(Invoice))
+    end
     
+  end
+
+  describe "finance_customer" do
+
+    before do 
+
+      @invoices = @invoices_params
+
+      it "should return the Customer line of credit updated " do
+        expect(finance_customer(@invoices_params, @customer_params)).to eq 7000
+      end
+        
+      it "should return the Customer line of credit updated " do
+        expect(get_processed_invoices).to include({"inv-burger1" => 15000, "inv-burger2" => 2000, "inv-burrito1" => 0, "inv-kola1" => 16000, "inv-kola2" => 0})
+      end
+
+    end
+
   end
 
 end
--- src/main.rb
+++ src/main.rb
@@ -1,11 +1,21 @@
 module Main
 
-  def load_invoices(invoices_hash)
-    return []
+  def load_invoices(data_invoices_hash)
+    invoices = []
+    data_invoices_hash.each do |invoice|
+      invoices << Invoice.new(invoice['debtor'],invoice['number'],invoice['amount'])
+    end
+    return invoices
   end
 
-  def load_customer(credit_line_hash)
-    return []
+  def load_customer(data_credit_line_hash)
+    return Customer.new(data_credit_line_hash['customer'])
+  end
+
+  def finance_customer(data_invoices_hash, data_credit_line_hash)
+    @invoices = load_invoices(data_invoices_hash)
+    customer = load_customer(data_credit_line_hash)
+    return finance_invoices(customer,  @invoices)
   end
 
 end
