63f3df424a27f5666c85121d0164fddaa235d90b
Stella Miranda <stellamariam@gmail.com>
2018-08-06 03:47:28 -0500

    Improve CLI and add sample output

--- cli.rb
+++ cli.rb
@@ -5,6 +5,11 @@ require './src/main'
 
 include Main
 
+if ARGV.length < 2
+  puts "We need the parameters to the files"
+  exit
+end
+
 def read_file(path) 
   begin
     file = File.open(path)
@@ -18,6 +23,16 @@ def read_file(path)
   return hash
 end
 
-updated_credit_line = finance_customer(read_file(File.open(ARGV[0])), read_file(File.open(ARGV[1])) )
+credit_line = read_file(File.open(ARGV[0]))
+invoices = read_file(File.open(ARGV[1])) 
 
-puts get_processed_invoices
+updated_credit_line = finance_customer(credit_line, invoices )
+
+puts "\n\n • Financed amount:"
+puts "\n " + credit_line['customer'].to_s
+
+puts "\n\n • Financed amount of each invoice"
+puts "\n " + JSON.pretty_generate(get_processed_invoices)
+
+puts "\n\n • Remaining customer credit:"
+puts "\n " + updated_credit_line.to_s
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,4 +4,25 @@ require 'rspec'
 
 describe "CLI" do
 
+  describe "Output" do 
+    
+    it "Should show an error message and exit when no argument passed" do 
+      sample = "We need the parameters to the files\n"
+      output = `./cli.rb`
+      expect(output).to eq sample
+    end 
+
+    it "Should show an error message and exit when only one argument passed" do 
+      sample = "We need the parameters to the files\n"
+      output = `./cli.rb data/credit_line.json`
+      expect(output).to eq sample
+    end 
+
+    it "Should match sample of example files given as argument" do 
+      sample = File.read("./spec/samples/output")
+      output = `./cli.rb data/credit_line.json data/invoices.json`
+      expect(output).to eq sample
+    end 
+
+  end
 end
